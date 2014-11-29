d5f649b4a18e30c3964255797de090d73a3c7f18
Greg Fausak <lgfausak+github@gmail.com>
2014-11-28 18:34:10 -0600

    sync with pypi version: 0.1.86

--- sqlauth/scripts/basicrouter.py
+++ sqlauth/scripts/basicrouter.py
@@ -70,14 +70,14 @@ class SessionData(ApplicationSession):
             ses._transport.sendClose(code=3000,reason=six.u('killed'))
             return defer.succeed({ 'killed': sid })
 
-        def list_session_data(details = None):
-            log.msg("SessionData:list_session_data()")
-            return self.sessiondb.list()
+        def list_user_data(details = None):
+            log.msg("SESSIONDATA:list_user_data()")
+            return self.userdb.list()
 
-        reg = yield self.register(list_data, 'adm.session.list', options = RegisterOptions(details_arg = 'details'))
+        reg = yield self.register(list_session_data, 'adm.session.list', options = RegisterOptions(details_arg = 'details'))
         reg = yield self.register(kill_session, 'adm.session.kill', options = RegisterOptions(details_arg = 'details'))
 
-        reg = yield self.register(kill_session, 'adm.user.list', options = RegisterOptions(details_arg = 'details'))
+        reg = yield self.register(list_user_data, 'adm.user.list', options = RegisterOptions(details_arg = 'details'))
 
     def onLeave(self, details):
         log.msg("onLeave: {}".format(details))

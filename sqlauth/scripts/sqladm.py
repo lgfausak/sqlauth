686b7a0137f11208f09648af822d6d1335118b87
Greg Fausak <lgfausak+github@gmail.com>
2014-11-30 17:12:33 -0600

    sync with pypi version: 0.1.149

--- sqlauth/scripts/sqladm.py
+++ sqlauth/scripts/sqladm.py
@@ -178,17 +178,17 @@ def run():
                         help='action args, json format, default: ' + def_action_args)
 
     role_p = sp.add_parser('role')
-    role_p.add_argument('action', choices=['list', 'get', 'update', 'delete'], help='Role commands')
+    role_p.add_argument('action', choices=['list', 'get', 'add', 'update', 'delete'], help='Role commands')
     role_p.add_argument('-a', '--args', action='store', dest='action_args', default=def_action_args,
                         help='action args, json format, default: ' + def_action_args)
 
     role_p = sp.add_parser('topic')
-    role_p.add_argument('action', choices=['list', 'get', 'update', 'delete'], help='Topic commands')
+    role_p.add_argument('action', choices=['list', 'get', 'add', 'update', 'delete'], help='Topic commands')
     role_p.add_argument('-a', '--args', action='store', dest='action_args', default=def_action_args,
                         help='action args, json format, default: ' + def_action_args)
 
     role_p = sp.add_parser('activity')
-    role_p.add_argument('action', choices=['list', 'get', 'update', 'delete'], help='Activity commands')
+    role_p.add_argument('action', choices=['list', 'get', 'add', 'update', 'delete'], help='Activity commands')
     role_p.add_argument('-a', '--args', action='store', dest='action_args', default=def_action_args,
                         help='action args, json format, default: ' + def_action_args)
 

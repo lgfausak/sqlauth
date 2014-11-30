f16ad84199316f827530508ea436741ecb6b6e46
Greg Fausak <lgfausak+github@gmail.com>
2014-11-29 22:16:36 -0600

    sync with pypi version: 0.1.121

--- sqlauth/scripts/sqladm.py
+++ sqlauth/scripts/sqladm.py
@@ -173,6 +173,11 @@ def run():
     role_p.add_argument('-a', '--args', action='store', dest='action_args', default=def_action_args,
                         help='action args, json format, default: ' + def_action_args)
 
+    role_p = sp.add_parser('activity')
+    role_p.add_argument('action', choices=['list', 'get', 'update', 'delete'], help='Activity commands')
+    role_p.add_argument('-a', '--args', action='store', dest='action_args', default=def_action_args,
+                        help='action args, json format, default: ' + def_action_args)
+
     args = p.parse_args()
     if args.verbose:
        log.startLogging(sys.stdout)
